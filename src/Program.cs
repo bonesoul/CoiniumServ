6a235b6d23d665b43b8b539317fe9fd014ebb2cf
Huseyin Uslu <shalafiraistlin@gmail.com>
2013-12-06 11:38:26 +0200

    Added logging support & startup banner.

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿/*
+ * Coinium project, Copyright (C) 2013, Int6 Studios - All Rights Reserved. - http://www.coinium.org
+ *
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Serilog;
+using coinium_serv.Utility;
 
 namespace coinium_serv
 {
@@ -10,6 +18,54 @@ namespace coinium_serv
     {
         static void Main(string[] args)
         {
+            #if !DEBUG
+                AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler; // Catch any unhandled exceptions.
+            #endif
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            ConsoleWindow.PrintBanner();
+            ConsoleWindow.PrintLicense();
+            Console.ResetColor();
+
+            InitLogging();
+            Log.Information("coinium-serv {0} warming-up..", Assembly.GetAssembly(typeof(Program)).GetName().Version);
+
+            Console.ReadLine();
         }
+
+        #region logging facility
+
+        private static void InitLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+        }
+
+        #endregion
+
+        #region unhandled exception emitter
+
+        /// <summary>
+        /// Unhandled exception emitter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = e.ExceptionObject as Exception;
+
+            if (exception == null) // if we can't get the exception, whine about it.
+                throw new ArgumentNullException("e");
+
+            Console.WriteLine(
+                    e.IsTerminating ? "Terminating because of unhandled exception: {0}" : "Caught unhandled exception: {0}",
+                    exception);
+
+            Console.ReadLine();
+        }
+
+        #endregion
     }
 }
---
+++ src/Utility/ConsoleWindow.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Coinium project, Copyright (C) 2013, Int6 Studios - All Rights Reserved. - http://www.coinium.org
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinium_serv.Utility
+{
+    /// <summary>
+    /// Utility class to handle console window stuff.
+    /// </summary>
+    class ConsoleWindow
+    {
+        /// <summary>
+        /// Prints an info banner.
+        /// </summary>
+        public static void PrintBanner()
+        {            
+            Console.WriteLine(@"             .__       .__               ");
+            Console.WriteLine(@"  ____  ____ |__| ____ |__|__ __  _____  ");
+            Console.WriteLine(@"_/ ___\/  _ \|  |/    \|  |  |  \/     \ ");
+            Console.WriteLine(@"\  \__(  <_> )  |   |  \  |  |  /  Y Y  \");
+            Console.WriteLine(@" \___  >____/|__|___|  /__|____/|__|_|  /");
+            Console.WriteLine(@"     \/              \/               \/ ");
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Prints a copyright banner.
+        /// </summary>
+        public static void PrintLicense()
+        {
+            Console.WriteLine("Copyright (C) 2013, Coinium project");
+            Console.WriteLine("Coinium-serv comes with ABSOLUTELY NO WARRANTY.");
+            Console.WriteLine("This is free software, and you are welcome to redistribute it under certain conditions; see the LICENSE file for details.");
+            Console.WriteLine();
+        }
+    }
+}
