dda5a79a80d4784c7385d6c21b408a5e21a3edb0
Huseyin Uslu <shalafiraistlin@gmail.com>
2013-12-06 11:20:29 +0200

    Pushing project structure.

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinium_serv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
