5e07c8cb9774f12a70573f55e4e2d69ea71337e9
Hüseyin Uslu <shalafiraistlin@gmail.com>
2014-02-28 22:17:05 +0200

    Added stats protocol for the server.

---
+++ src/Servers/Web/Protocols/Stats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Nancy;
+using Serilog;
+
+namespace Coinium.Servers.Web.Modules
+{
+    public class Stats : NancyModule
+    {
+        public Stats()
+        {
+            Get["/"] = @params => "coinium";
+
+            var banner = string.Format("v{0}", Assembly.GetAssembly(typeof(Program)).GetName().Version);
+            Get["/version"] = x => banner;
+        }
+    }
+}
