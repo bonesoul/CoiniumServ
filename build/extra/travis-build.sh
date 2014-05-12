be99e20912bac4782fe34ea3e51e37b4b1128e52
Hüseyin Uslu <shalafiraistlin@gmail.com>
2014-05-12 17:33:26 +0300

    Update and rename build/travis-build.sh to build/extra/travis-build.sh

--- build/extra/travis-build.sh
+++ build/extra/travis-build.sh
@@ -1,3 +1,4 @@
 #/usr/bin/env/sh
 hdid "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg"
 sudo installer -pkg "/Volumes/Mono Framework MDK ${MONO_VER}/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
+sudo installer -pkg "MonoFramework-MDK-${MONO_VERSION}.macos10.xamarin.x86.pkg" -target /
