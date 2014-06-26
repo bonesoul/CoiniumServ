d8bbdebb791fbc4f41a225ad14c42e5352d5bb46
bonesoul <shalafiraistlin@gmail.com>
2014-06-26 13:33:42 +0300

    Updated license texts.

--- src/CoiniumServ/Mining/Pools/Config/IPoolConfigFactory.cs
+++ src/CoiniumServ/Mining/Pools/Config/IPoolConfigFactory.cs
@@ -1,4 +1,25 @@
-﻿namespace Coinium.Mining.Pools.Config
+﻿#region License
+// 
+//     CoiniumServ - crypto currency pool software - https://github.com/CoiniumServ/CoiniumServ
+//     Copyright (C) 2013 - 2014, Coinium Project - http://www.coinium.org
+// 
+//     This software is dual-licensed: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//    
+//     For the terms of this license, see licenses/gpl_v3.txt.
+// 
+//     Alternatively, you can license this software under a commercial
+//     license or white-label it as set out in licenses/commercial.txt.
+// 
+#endregion
+namespace Coinium.Mining.Pools.Config
 {
     /// <summary>
     /// Returns an instance of IPoolConfig based on the parsed config file
