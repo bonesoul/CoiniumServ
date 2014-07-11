fd4f5d6743a2a107c0efcb6298be04b59059acd6
bonesoul <shalafiraistlin@gmail.com>
2014-07-11 15:00:27 +0300

    Updated packages.

--- src/Tests/Mining/Pools/PoolTests.cs
+++ src/Tests/Mining/Pools/PoolTests.cs
@@ -27,13 +27,13 @@ using Coinium.Mining.Jobs.Manager;
 using Coinium.Mining.Jobs.Tracker;
 using Coinium.Mining.Miners;
 using Coinium.Mining.Pools.Config;
+using Coinium.Mining.Pools.Statistics;
 using Coinium.Mining.Shares;
 using Coinium.Payments;
 using Coinium.Persistance;
 using Coinium.Server;
 using Coinium.Server.Mining;
 using Coinium.Service;
-using Coinium.Utils.Configuration;
 using NSubstitute;
 using Should.Fluent;
 using Xunit;
@@ -52,6 +52,8 @@ namespace Tests.Mining.Pools
         private readonly IMinerManagerFactory _minerManagerFactory;
         private readonly IStorageFactory _storageFactory;
         private readonly IPaymentProcessorFactory _paymentProcessorFactory;
+        private readonly IPoolStatisticsFactory _poolStatisticsFactory;
+        private readonly IBlockStatisticsFactory _blockStatisticsFactory;
 
         // object mocks.
         private readonly IDaemonClient _daemonClient;
@@ -63,6 +65,8 @@ namespace Tests.Mining.Pools
         private readonly IMiningServer _miningServer;
         private readonly IRpcService _rpcService;
         private readonly IPaymentProcessor _paymentProcessor;
+        private readonly IPoolStatistics _poolStatistics;
+        private readonly IBlockStatistics _blockStatistics;
 
         /// <summary>
         /// Initialize mock objects.
@@ -78,6 +82,8 @@ namespace Tests.Mining.Pools
             _serviceFactory = Substitute.For<IServiceFactory>();
             _storageFactory = Substitute.For<IStorageFactory>();
             _paymentProcessorFactory = Substitute.For<IPaymentProcessorFactory>();
+            _poolStatisticsFactory = Substitute.For<IPoolStatisticsFactory>();
+            _blockStatisticsFactory = Substitute.For<IBlockStatisticsFactory>();
 
             _daemonClient = Substitute.For<IDaemonClient>();
             _minerManager = Substitute.For<IMinerManager>();
@@ -88,6 +94,8 @@ namespace Tests.Mining.Pools
             _rpcService = Substitute.For<IRpcService>();
             _storage = Substitute.For<IStorage>();
             _paymentProcessor = Substitute.For<IPaymentProcessor>();
+            _poolStatistics = Substitute.For<IPoolStatistics>();
+            _blockStatistics = Substitute.For<IBlockStatistics>();
         }
 
         /// <summary>
@@ -106,263 +114,15 @@ namespace Tests.Mining.Pools
                 _jobManagerFactory,
                 _shareManagerFactory,
                 _storageFactory,
-                _paymentProcessorFactory
+                _paymentProcessorFactory,
+                _poolStatisticsFactory,
+                _blockStatisticsFactory
                 );
 
             pool.Should().Not.Be.Null();
             pool.InstanceId.Should().Be.GreaterThan((UInt32)0);
         }
-
-        /// <summary>
-        /// Tests pool constructor with null HashAlgorithm, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullHashAlgorithmFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    null,
-                    _serverFactory,
-                    _serviceFactory,
-                    _daemonClient,
-                    _minerManagerFactory,
-                    _jobTrackerFactory,
-                    _jobManagerFactory,
-                    _shareManagerFactory,
-                    _storageFactory,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IHashAlgorithmFactory");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null ServerFactory, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullServerFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    null,
-                    _serviceFactory,
-                    _daemonClient,
-                    _minerManagerFactory,
-                    _jobTrackerFactory,
-                    _jobManagerFactory,
-                    _shareManagerFactory,
-                    _storageFactory,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IServerFactory");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null ServiceFactory, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullServiceFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    _serverFactory,
-                    null,
-                    _daemonClient,
-                    _minerManagerFactory,
-                    _jobTrackerFactory,
-                    _jobManagerFactory,
-                    _shareManagerFactory,
-                    _storageFactory,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IServiceFactory");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null DaemonClient, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullDaemonClient_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    _serverFactory,
-                    _serviceFactory,
-                    null,
-                    _minerManagerFactory,
-                    _jobTrackerFactory,
-                    _jobManagerFactory,
-                    _shareManagerFactory,
-                    _storageFactory,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IDaemonClient");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null IMinerManagerFactory, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullMinerManagerFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    _serverFactory,
-                    _serviceFactory,
-                    _daemonClient,
-                    null,
-                    _jobTrackerFactory,
-                    _jobManagerFactory,
-                    _shareManagerFactory,
-                    _storageFactory,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IMinerManagerFactory");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null JobManager, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullJobTrackerFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    _serverFactory,
-                    _serviceFactory,
-                    _daemonClient,
-                    _minerManagerFactory,
-                    null,
-                    _jobManagerFactory,
-                    _shareManagerFactory,
-                    _storageFactory,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IJobTrackerFactory");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null JobManager, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullJobManagerFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    _serverFactory,
-                    _serviceFactory,
-                    _daemonClient,
-                    _minerManagerFactory,
-                    _jobTrackerFactory,
-                    null,
-                    _shareManagerFactory,
-                    _storageFactory,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IJobManagerFactory");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null ShareManager, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullShareManagerFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    _serverFactory,
-                    _serviceFactory,
-                    _daemonClient,
-                    _minerManagerFactory,
-                    _jobTrackerFactory,
-                    _jobManagerFactory,
-                    null,
-                    _storageFactory,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IShareManagerFactory");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null StorageFactory, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullStorageFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    _serverFactory,
-                    _serviceFactory,
-                    _daemonClient,
-                    _minerManagerFactory,
-                    _jobTrackerFactory,
-                    _jobManagerFactory,
-                    _shareManagerFactory,
-                    null,
-                    _paymentProcessorFactory
-                    );
-            });
-
-            ex.Message.Should().Contain("IStorageFactory");
-        }
-
-        /// <summary>
-        /// Tests pool constructor with null StorageFactory, should trow exception.
-        /// </summary>
-        [Fact]
-        public void ConstructorTest_NullPaymentProcessorFactory_ShouldThrow()
-        {
-            Exception ex = Assert.Throws<ArgumentNullException>(() =>
-            {
-                var pool = new Coinium.Mining.Pools.Pool(
-                    _hashAlgorithmFactory,
-                    _serverFactory,
-                    _serviceFactory,
-                    _daemonClient,
-                    _minerManagerFactory,
-                    _jobTrackerFactory,
-                    _jobManagerFactory,
-                    _shareManagerFactory,
-                    _storageFactory,
-                    null
-                    );
-            });
-
-            ex.Message.Should().Contain("IPaymentProcessorFactory");
-        }
-
+  
         /// <summary>
         /// Initializes pool with all valid parameters, should succeed.
         /// </summary>
@@ -379,7 +139,9 @@ namespace Tests.Mining.Pools
                 _jobManagerFactory, 
                 _shareManagerFactory,
                 _storageFactory,
-                _paymentProcessorFactory
+                _paymentProcessorFactory,
+                _poolStatisticsFactory,
+                _blockStatisticsFactory
                 );
 
             pool.Should().Not.Be.Null();
