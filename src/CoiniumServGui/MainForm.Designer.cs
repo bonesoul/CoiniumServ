07794b9501ceec3b3facb4aefc2804b0722bf99b
Hüseyin Uslu <shalafiraistlin@gmail.com>
2014-09-12 01:40:49 +0300

    removed website tab from coiniumserv-gui.

--- src/CoiniumServGui/MainForm.Designer.cs
+++ src/CoiniumServGui/MainForm.Designer.cs
@@ -31,11 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.consoleControl1 = new ConsoleControl.ConsoleControl();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPageWeb = new System.Windows.Forms.TabPage();
             this.tabPageConsole = new System.Windows.Forms.TabPage();
-            this.webBrowser = new System.Windows.Forms.WebBrowser();
             this.tabControl1.SuspendLayout();
-            this.tabPageWeb.SuspendLayout();
             this.tabPageConsole.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,7 +49,6 @@
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPageWeb);
             this.tabControl1.Controls.Add(this.tabPageConsole);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
@@ -60,17 +56,6 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(829, 512);
             this.tabControl1.TabIndex = 1;
-            // 
-            // tabPageWeb
-            // 
-            this.tabPageWeb.Controls.Add(this.webBrowser);
-            this.tabPageWeb.Location = new System.Drawing.Point(4, 22);
-            this.tabPageWeb.Name = "tabPageWeb";
-            this.tabPageWeb.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageWeb.Size = new System.Drawing.Size(821, 486);
-            this.tabPageWeb.TabIndex = 0;
-            this.tabPageWeb.Text = "Site";
-            this.tabPageWeb.UseVisualStyleBackColor = true;
             // 
             // tabPageConsole
             // 
@@ -83,16 +68,6 @@
             this.tabPageConsole.Text = "Console";
             this.tabPageConsole.UseVisualStyleBackColor = true;
             // 
-            // webBrowser
-            // 
-            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser.Location = new System.Drawing.Point(3, 3);
-            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser.Name = "webBrowser";
-            this.webBrowser.Size = new System.Drawing.Size(815, 480);
-            this.webBrowser.TabIndex = 0;
-            this.webBrowser.Url = new System.Uri("http://127.0.0.1", System.UriKind.Absolute);
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -101,10 +76,9 @@
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.Text = "CoiniumServ";
+            this.Text = "CoiniumServ GUI";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.tabControl1.ResumeLayout(false);
-            this.tabPageWeb.ResumeLayout(false);
             this.tabPageConsole.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -114,9 +88,7 @@
 
         private ConsoleControl.ConsoleControl consoleControl1;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPageWeb;
         private System.Windows.Forms.TabPage tabPageConsole;
-        private System.Windows.Forms.WebBrowser webBrowser;
     }
 }
 
