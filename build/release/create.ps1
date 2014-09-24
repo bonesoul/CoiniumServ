372c4cb201e49c20ed0be5b5bf5880879d323387
bonesoul <shalafiraistlin@gmail.com>
2014-09-24 19:47:37 +0300

    fixed release script.

--- build/release/create.ps1
+++ build/release/create.ps1
@@ -2,6 +2,6 @@ $dotNetVersion = "4.0"
 $regKey = "HKLM:\software\Microsoft\MSBuild\ToolsVersions\$dotNetVersion"
 $regProperty = "MSBuildToolsPath"
 $msbuildExe = join-path -path (Get-ItemProperty $regKey).$regProperty -childpath "msbuild.exe"
-cd ../../../
-&$msbuildExe build/CoiniumServ.sln /p:Configuration=Release /t:rebuild /p:DebugSymbols=false /p:DebugType=None /p:AllowedReferenceRelatedFileExtensions=none
-cp src/CoiniumServ/bin/Nancy.ViewEngines.Razor.dll bin/Release/Nancy.ViewEngines.Razor.dll
+&$msbuildExe ../../build/CoiniumServ.sln /p:Configuration=Release /t:rebuild /p:DebugSymbols=false /p:DebugType=None /p:AllowedReferenceRelatedFileExtensions=none
+cp ../../src/CoiniumServ/bin/Nancy.ViewEngines.Razor.dll ../../bin/Release/Nancy.ViewEngines.Razor.dll
+../../contrib/xunit/xunit.console.clr4.x86.exe ../../src/Tests/bin/Release/CoiniumServ.Tests.dll
