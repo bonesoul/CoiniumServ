8f7593fda2c4800e03e75578d1421854fa6c25b4
Vagabondan <anj5@mail.ru>
2017-08-08 18:02:36 +0300

    Update build.sh

--- build/release/build.sh
+++ build/release/build.sh
@@ -3,5 +3,5 @@ cd ../..
 git submodule init
 git submodule update
 mozroots --import --ask-remove
-xbuild src/CoiniumServ.sln /p:Configuration="Release"
+xbuild src/CoiniumServ/CoiniumServ.sln /p:Configuration="Release"
 mono contrib/xunit/xunit.console.clr4.x86.exe src/Tests/bin/Release/CoiniumServ.Tests.dll
