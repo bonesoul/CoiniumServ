f4f083e7ed478ee1f955896cc91feb01430471dd
Viktor Kynchev <v.kynchev@gmail.com>
2017-08-18 19:18:39 +0300

    Updated socket disconnect exception...

--- src/CoiniumServ/Server/Mining/Stratum/Sockets/SocketServer.cs
+++ src/CoiniumServ/Server/Mining/Stratum/Sockets/SocketServer.cs
@@ -304,7 +304,9 @@ namespace CoiniumServ.Server.Mining.Stratum.Sockets
                     try
                     {
                         connection.Socket.Disconnect(true);
-                    }catch(Exception ex){
+                    }
+                    catch(Exception ex)
+                    {
                         _logger.Error(ex,"Exception on client disconnection");
                     }
                 }
