b9c42f3f233a85928273998bc71f455e5c788bfb
Andrew Denner <denner@gmail.com>
2017-11-02 00:49:58 -0500

    mozroots no longer needed
    Mozroots are not needed any longer in modern versions of mono

--- build/debug/build.sh
+++ build/debug/build.sh
@@ -2,6 +2,5 @@
 cd ../..
 git submodule init
 git submodule update
-mozroots --import --ask-remove
 xbuild build/CoiniumServ.sln /p:Configuration="Debug"
 mono contrib/xunit/xunit.console.clr4.x86.exe src/Tests/bin/Debug/CoiniumServ.Tests.dll
