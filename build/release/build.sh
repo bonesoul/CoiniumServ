a7e923a1451a3321fe4aaf3e3742e06b3c730fe6
huseyin uslu <shalafiraistlin@gmail.com>
2017-12-21 15:46:53 +0300

    porting to dotnet core 2.0.

--- build/release/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ../..
-git submodule init
-git submodule update
-xbuild build/CoiniumServ.sln /p:Configuration="Release"
-mono contrib/xunit/xunit.console.clr4.x86.exe src/Tests/bin/Release/CoiniumServ.Tests.dll
--- src/CoiniumServ/Server/Commands/Stats.cs
+++ src/CoiniumServ/Server/Commands/Stats.cs
@@ -44,38 +44,38 @@ namespace CoiniumServ.Server.Commands
 
             output.AppendFormat("GC Allocated Memory: {0}KB ", GC.GetTotalMemory(true) / 1024);
 
-            if (PerformanceCounterCategory.Exists("Processor") && PerformanceCounterCategory.CounterExists("% Processor Time", "Processor"))
-            {
-                var processorTimeCounter = new PerformanceCounter { CategoryName = "Processor", CounterName = "% Processor Time", InstanceName = "_Total" };
-                output.AppendFormat("Processor Time: {0}%", processorTimeCounter.NextValue());
-            }
+            //if (PerformanceCounterCategory.Exists("Processor") && PerformanceCounterCategory.CounterExists("% Processor Time", "Processor"))
+            //{
+            //    var processorTimeCounter = new PerformanceCounter { CategoryName = "Processor", CounterName = "% Processor Time", InstanceName = "_Total" };
+            //    output.AppendFormat("Processor Time: {0}%", processorTimeCounter.NextValue());
+            //}
 
-            if (PerformanceCounterCategory.Exists(".NET CLR LocksAndThreads"))
-            {
-                if (PerformanceCounterCategory.CounterExists("# of current physical Threads", ".NET CLR LocksAndThreads"))
-                {
-                    var physicalThreadsCounter = new PerformanceCounter { CategoryName = ".NET CLR LocksAndThreads", CounterName = "# of current physical Threads", InstanceName = Process.GetCurrentProcess().ProcessName };
-                    output.AppendFormat("\nPhysical Threads: {0} ", physicalThreadsCounter.NextValue());
-                }
+            //if (PerformanceCounterCategory.Exists(".NET CLR LocksAndThreads"))
+            //{
+            //    if (PerformanceCounterCategory.CounterExists("# of current physical Threads", ".NET CLR LocksAndThreads"))
+            //    {
+            //        var physicalThreadsCounter = new PerformanceCounter { CategoryName = ".NET CLR LocksAndThreads", CounterName = "# of current physical Threads", InstanceName = Process.GetCurrentProcess().ProcessName };
+            //        output.AppendFormat("\nPhysical Threads: {0} ", physicalThreadsCounter.NextValue());
+            //    }
 
-                if (PerformanceCounterCategory.CounterExists("# of current logical Threads", ".NET CLR LocksAndThreads"))
-                {
-                    var logicalThreadsCounter = new PerformanceCounter { CategoryName = ".NET CLR LocksAndThreads", CounterName = "# of current logical Threads", InstanceName = Process.GetCurrentProcess().ProcessName };
-                    output.AppendFormat("Logical Threads: {0} ", logicalThreadsCounter.NextValue());
-                }
+            //    if (PerformanceCounterCategory.CounterExists("# of current logical Threads", ".NET CLR LocksAndThreads"))
+            //    {
+            //        var logicalThreadsCounter = new PerformanceCounter { CategoryName = ".NET CLR LocksAndThreads", CounterName = "# of current logical Threads", InstanceName = Process.GetCurrentProcess().ProcessName };
+            //        output.AppendFormat("Logical Threads: {0} ", logicalThreadsCounter.NextValue());
+            //    }
 
-                if (PerformanceCounterCategory.CounterExists("Contention Rate / sec", ".NET CLR LocksAndThreads"))
-                {
-                    var contentionRateCounter = new PerformanceCounter { CategoryName = ".NET CLR LocksAndThreads", CounterName = "Contention Rate / sec", InstanceName = Process.GetCurrentProcess().ProcessName };
-                    output.AppendFormat("Contention Rate: {0}/sec", contentionRateCounter.NextValue());
-                }
-            }
+            //    if (PerformanceCounterCategory.CounterExists("Contention Rate / sec", ".NET CLR LocksAndThreads"))
+            //    {
+            //        var contentionRateCounter = new PerformanceCounter { CategoryName = ".NET CLR LocksAndThreads", CounterName = "Contention Rate / sec", InstanceName = Process.GetCurrentProcess().ProcessName };
+            //        output.AppendFormat("Contention Rate: {0}/sec", contentionRateCounter.NextValue());
+            //    }
+            //}
 
-            if (PerformanceCounterCategory.Exists(".NET CLR Exceptions") && PerformanceCounterCategory.CounterExists("# of Exceps Thrown", ".NET CLR Exceptions"))
-            {
-                var exceptionsThrownCounter = new PerformanceCounter { CategoryName = ".NET CLR Exceptions", CounterName = "# of Exceps Thrown", InstanceName = Process.GetCurrentProcess().ProcessName };
-                output.AppendFormat("\nExceptions Thrown: {0}", exceptionsThrownCounter.NextValue());
-            }
+            //if (PerformanceCounterCategory.Exists(".NET CLR Exceptions") && PerformanceCounterCategory.CounterExists("# of Exceps Thrown", ".NET CLR Exceptions"))
+            //{
+            //    var exceptionsThrownCounter = new PerformanceCounter { CategoryName = ".NET CLR Exceptions", CounterName = "# of Exceps Thrown", InstanceName = Process.GetCurrentProcess().ProcessName };
+            //    output.AppendFormat("\nExceptions Thrown: {0}", exceptionsThrownCounter.NextValue());
+            //}
 
             return output.ToString();
         }
