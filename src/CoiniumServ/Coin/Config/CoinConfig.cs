7da223752275875028cc71584c4cd42b637e0355
globaltoken <globaltoken@users.noreply.github.com>
2018-03-13 16:16:10 +0100

    Update CoinConfig.cs

--- src/CoiniumServ/Coin/Config/CoinConfig.cs
+++ src/CoiniumServ/Coin/Config/CoinConfig.cs
@@ -61,7 +61,7 @@ namespace CoiniumServ.Coin.Config
                 Symbol = config.symbol;
                 Algorithm = config.algorithm;
                 Site = string.IsNullOrEmpty(config.site) ? string.Empty : config.site;
-                RpcUpdate = RpcUpdate.HasValue ? false : config.RpcUpdate;
+                RpcUpdate = RpcUpdate.HasValue ? config.rpcupdate : false;
                 Options = new CoinOptions(config.options);
                 BlockExplorer = new BlockExplorerOptions(config.blockExplorer);
                 Extra = config.extra;
